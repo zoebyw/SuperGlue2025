5f5886a396192c8f9a523f8d6e388c8e4ff43fa2
Mosalah11-red <haoyuhe@uw.edu>
2025-02-11 15:10:29 -0800

    更新描述

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/main.css'; // 确保引入你的全局样式
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
